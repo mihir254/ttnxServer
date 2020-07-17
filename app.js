284fc36e0634f66cdf7ec9100bc4d2dbe81aec97
Mihir Bhansali <mbmihirbhansali@gmail.com>
2020-07-17 22:24:53 +0530

    minors

--- app.js
+++ app.js
@@ -54,12 +54,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Cors
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+
+app.use(cors());
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
