0ccc88fb4632d0c91ca770b3cd6f6b0d8e659f3c
Mihir Bhansali <mbmihirbhansali@gmail.com>
2020-07-19 15:05:22 +0530

    add picture

--- routes/admin.js
+++ routes/admin.js
@@ -53,10 +53,18 @@ router
   });
 
 //all about products
-router.route("/products").post((req, res, next) => {
-  Product.create({
+router.post("/products", upload.array("myImages"), (req, res, next) => {
+  let paths = [];
+  for (let i = 0; i < req.files.length; i++) {
+    paths.push(req.files[i].path.replace(/\\/g, "/"));
+  }
+  let product = {
     ...req.body,
-  })
+    images: paths,
+    colors: req.body.colors.split(","),
+    size: req.body.size.split(","),
+  };
+  Product.create(product)
     .then((prod) => {
       return Product.findById(prod._id);
     })
